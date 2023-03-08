f0283f5cd7ba2446e648ed752c4ffed6a560bfd5
vedansh kumar <56169665+vedanshk@users.noreply.github.com>
2023-03-08 20:30:40 +0530

    added middleware

--- src/Root.js
+++ src/Root.js
@@ -2,12 +2,12 @@ import React from "react";
 import { Provider } from "react-redux";
 import { applyMiddleware } from "redux";
 import {createStore} from 'redux';
-import reduxPromise from 'redux-promise';
+import async from "./middleware/async";
 import reducers from "./reducers";
 
 
 export default ({children , initialState={} }) =>{
-    const store = createStore(reducers , initialState , applyMiddleware(reduxPromise));
+    const store = createStore(reducers , initialState , applyMiddleware(async));
     return (
         <Provider store={store} >
             {children}
